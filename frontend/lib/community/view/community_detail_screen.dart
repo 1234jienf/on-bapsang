3c0b399c3b022bc231475579a4148fa17d740932
Hocheol <didifia9850@naver.com>
2025-05-27 19:46:46 +0900

    버그 수정 및 커뮤니티 제작

--- frontend/lib/community/view/community_detail_screen.dart
+++ frontend/lib/community/view/community_detail_screen.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/common/layout/default_layout.dart';
 
 class CommunityDetailScreen extends StatelessWidget {
   final String id;
+
   static String get routeName => 'CommunityDetailScreen';
 
   const CommunityDetailScreen({super.key, required this.id});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultLayout(
+      appBar: AppBar(backgroundColor: Colors.white, elevation: 0),
+      child: Center(child: Text('detail')),
+    );
   }
 }
