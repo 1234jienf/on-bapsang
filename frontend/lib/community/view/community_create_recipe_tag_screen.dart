b750bf1e2c3b8f64f29ad63785c1a0b38be93dba
Hocheol <didifia9850@naver.com>
2025-05-28 19:20:50 +0900

    커뮤니티 제작

---
+++ frontend/lib/community/view/community_create_recipe_tag_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CommunityCreateRecipeTagScreen extends StatefulWidget {
+  const CommunityCreateRecipeTagScreen({super.key});
+
+  @override
+  State<CommunityCreateRecipeTagScreen> createState() =>
+      _CommunityCreateRecipeTagScreenState();
+}
+
+class _CommunityCreateRecipeTagScreenState
+    extends State<CommunityCreateRecipeTagScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ frontend/lib/community/view/community_create_upload_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CommunityCreateUploadScreen extends StatelessWidget {
+  const CommunityCreateUploadScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
