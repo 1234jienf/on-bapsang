a32ea83dbc7826a28bc1b844a74abd03d5ae7278
Hocheol <didifia9850@naver.com>
2025-05-29 23:41:05 +0900

    커뮤니티 제작

--- frontend/lib/community/view/community_create_upload_screen.dart
+++ frontend/lib/community/view/community_create_upload_screen.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class CommunityCreateUploadScreen extends StatelessWidget {
+class CommunityCreateUploadScreen extends StatefulWidget {
   const CommunityCreateUploadScreen({super.key});
+
+  @override
+  State<CommunityCreateUploadScreen> createState() =>
+      _CommunityCreateUploadScreenState();
+}
+
+class _CommunityCreateUploadScreenState
+    extends State<CommunityCreateUploadScreen> {
 
   @override
   Widget build(BuildContext context) {
