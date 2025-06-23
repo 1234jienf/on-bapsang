911b75e3233f07b3053c2e47a0f551de82de46b3
Hocheol <didifia9850@naver.com>
2025-06-23 19:02:08 +0900

    fix

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -1,20 +1,50 @@
+import java.util.Properties
+
+val keystoreProperties = Properties().apply {
+    val keystoreFile = file("../key.properties")
+    if (keystoreFile.exists()) {
+        keystoreFile.inputStream().use { this.load(it) }
+    }
+}
+
+val dotenv = Properties().apply {
+    val envFile = file("../../assets/config/.env")
+    if (envFile.exists()) {
+        envFile.inputStream().use { this.load(it) }
+    }
+}
+
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
-import java.util.Properties
-import java.io.FileInputStream
-
-val dotenv = Properties()
-val envFile = file("../../assets/config/.env")
-if (envFile.exists()) {
-    dotenv.load(FileInputStream(envFile))
-}
-
 android {
+
+    signingConfigs {
+        create("release") {
+            println("storeFile: ${keystoreProperties["storeFile"]}")
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
     namespace = "com.example.frontend"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
@@ -49,13 +79,6 @@ android {
         resValue("string", "google_maps_api_key", dotenv.getProperty("GOOGLE_MAPS_API_KEY", ""))
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
 }
 
 flutter {
